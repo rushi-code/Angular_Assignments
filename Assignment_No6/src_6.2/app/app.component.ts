b12eaf0302807c38f8d9ae06dfee74e00ceb5ffb
Rushikesh Ingale <127707145+rushi-code@users.noreply.github.com>
2023-04-19 15:45:31 +0530

    Assignment_No6

---
+++ Assignment_No6/src_6.2/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <div style="text-align: center;">
+
+<h1 >"Marvellous Infosystems"</h1>
+<input type="text">
+
+</div>
+  `,
+
+  
+  styles: [`
+ 
+      h1
+      {
+        color:blue;
+      }
+  `]
+})
+export class AppComponent {
+  title = 'Assignment_No6';
+}
