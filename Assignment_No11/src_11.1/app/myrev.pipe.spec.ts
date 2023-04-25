1c6e831492c8193a87c7342965b9080a98451318
Rushikesh Ingale <127707145+rushi-code@users.noreply.github.com>
2023-04-25 13:10:53 +0530

    Delete Assignment_No11 directory

--- Assignment_No11/src_11.1/app/myrev.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MyrevPipe } from './myrev.pipe';
-
-describe('MyrevPipe', () => {
-  it('create an instance', () => {
-    const pipe = new MyrevPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
